e62d803e1f81e7d567618d39617e9575c56354b3
An Pham <an.pham@netpower.no>
2021-03-02 10:15:11 +0700

    adjust formatresponse to check metadata

--- src/interceptors/format-response.ts
+++ src/interceptors/format-response.ts
@@ -4,15 +4,28 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { METADATA } from '../audit-log';
 
 @Injectable()
 export class FormatResponseInterceptor implements NestInterceptor {
+  constructor(private reflector: Reflector | null = null) {
+    this.reflector = reflector;
+  }
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    let willFormat = !(
+      this.reflector &&
+      this.reflector.get<string>(
+        METADATA.EXCEPTION_FORMAT_ROUTES,
+        context.getHandler(),
+      )
+    );
+
     return next.handle().pipe(
       map((data) => {
-        return { success: true, result: data };
+        return willFormat ? { success: true, result: data } : data;
       }),
     );
   }
