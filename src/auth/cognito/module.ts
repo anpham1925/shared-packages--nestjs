1dc3095e216f08badf888c442a0ac7a08629d6a5
An Pham <anpvt@ahamove.com>
2021-11-09 16:28:45 +0700

    update

--- src/auth/cognito/module.ts
+++ src/auth/cognito/module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { CognitoService } from './cognito';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [],
   controllers: [],
   providers: [CognitoService],
   exports: [CognitoService],
